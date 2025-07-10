6e9a43bf3ccc45c3cadf964fe929c9711155b78e
baozy2000 <baozy2000@163.com>
2025-07-10 14:45:00 +0800

    HttpRequestParser

---
+++ http_parser.cpp
@@ -0,0 +1,149 @@
+#include "http_parser.h"
+#include "log.h"
+#include "config.h"
+
+#include <string.h>
+
+namespace Framework {
+    namespace HTTP {
+		static Framework::Logger::ptr g_logger = LOG_NAME("system");
+
+		// 防止请求头和请求体过大，不然就当做是恶意请求
+		static Framework::ConfigVar<uint64_t>::ptr g_http_request_buffer_size =
+			Framework::Config::Lookup("http.request.buffer_size", (uint64_t)(4 * 1024), "http request buffer size");
+
+		static Framework::ConfigVar<uint64_t>::ptr g_http_request_max_body_size =
+			Framework::Config::Lookup("http.request.max_body_size", (uint64_t)(64 * 1024 * 1024), "http request max body size");
+
+        // 确保这些配置项初始化
+        static uint64_t s_http_request_buffer_size = 0;
+        static uint64_t s_http_request_max_body_size = 0;
+        struct _RequestSizeIniter {
+            _RequestSizeIniter() {
+                s_http_request_buffer_size = g_http_request_buffer_size->getValue();
+                s_http_request_max_body_size = g_http_request_max_body_size->getValue();
+                g_http_request_buffer_size->addListener(
+                    [](const uint64_t& ov, const uint64_t& nv) {
+                        s_http_request_buffer_size = nv;
+                    });
+
+                g_http_request_max_body_size->addListener(
+                    [](const uint64_t& ov, const uint64_t& nv) {
+                        s_http_request_max_body_size = nv;
+                    });
+            }
+        };
+        static _RequestSizeIniter _init;
+    }
+
+    namespace HTTP {
+        void on_request_method(void* data, const char* at, size_t length) {
+            HttpRequestParser* parser = static_cast<HttpRequestParser*>(data);
+            HttpMethod m = CharsToHttpMethod(at);
+
+            if (m == HttpMethod::INVALID_METHOD) {
+                LOG_WARN(g_logger) << "invalid http request method " << std::string(at, length);
+                parser->setError(1000);
+                return;
+            }
+            parser->getData()->setMethod(m);
+        }
+
+        void on_request_uri(void* data, const char* at, size_t length) {
+
+        }
+
+        void on_request_fragment(void* data, const char* at, size_t length) {
+            HttpRequestParser* parser = static_cast<HttpRequestParser*>(data);
+            parser->getData()->setFragment(std::string(at, length));
+        }
+
+        void on_request_path(void* data, const char* at, size_t length) {
+            HttpRequestParser* parser = static_cast<HttpRequestParser*>(data);
+            parser->getData()->setPath(std::string(at, length));
+        }
+
+        void on_request_query(void* data, const char* at, size_t length) {
+            HttpRequestParser* parser = static_cast<HttpRequestParser*>(data);
+            parser->getData()->setQuery(std::string(at, length));
+        }
+
+        void on_request_version(void* data, const char* at, size_t length) {
+            HttpRequestParser* parser = static_cast<HttpRequestParser*>(data);
+            uint8_t v = 0;
+            if (strncmp(at, "HTTP/1.1", length) == 0) {
+                v = 0x11;
+            }
+            else if (strncmp(at, "HTTP/1.0", length) == 0) {
+                v = 0x10;
+            }
+            else {
+                LOG_WARN(g_logger) << "invalid http request version: " << std::string(at, length);
+                parser->setError(1001);
+                return;
+            }
+            parser->getData()->setVersion(v);
+        }
+
+        void on_request_header_done(void* data, const char* at, size_t length) {
+            //HttpRequestParser* parser = static_cast<HttpRequestParser*>(data);
+        }
+
+        void on_request_http_field(void* data, const char* field, size_t flen, const char* value, size_t vlen) {
+            HttpRequestParser* parser = static_cast<HttpRequestParser*>(data);
+            if (flen == 0) {
+                LOG_WARN(g_logger) << "invalid http request field length == 0";
+                parser->setError(1002);
+                return;
+            }
+            parser->getData()->setHeader(std::string(field, flen), std::string(value, vlen));
+        }
+
+        HttpRequestParser::HttpRequestParser() 
+            : m_data(0) {
+            m_data.reset(new Framework::HTTP::HttpRequest);
+            http_parser_init(&m_parser);
+            m_parser.request_method = on_request_method;
+            m_parser.request_uri = on_request_uri;
+            m_parser.fragment = on_request_fragment;
+            m_parser.request_path = on_request_path;
+            m_parser.query_string = on_request_query;
+            m_parser.http_version = on_request_version;
+            m_parser.header_done = on_request_header_done;
+            m_parser.http_field = on_request_http_field;
+            m_parser.data = this;
+        }
+
+        // 1: 成功
+        // -1: 有错误
+        // >0: 已处理的字节数，且data有效数据为len - v;
+        size_t HttpRequestParser::execute(char* data, size_t len) {
+            size_t offset = http_parser_execute(&m_parser, data, len, 0);
+            // 还没解析完就把已经解析的移走
+            memmove(data, data + offset, (len - offset));
+            return offset;
+        }
+
+        int HttpRequestParser::isFinished() {
+            return http_parser_finish(&m_parser);
+        }
+
+        int HttpRequestParser::hasError() {
+            return m_error || http_parser_has_error(&m_parser);
+        }
+    }
+
+    namespace HTTP {
+        HttpResponseParser::HttpResponseParser() {
+            m_data.reset(new Framework::HTTP::HttpResponse);
+            httpclient_parser_init(&m_parser);
+            m_parser.reason_phrase = on_response_reason;
+            m_parser.status_code = on_response_status;
+            m_parser.chunk_size = on_response_chunk;
+            m_parser.http_version = on_response_version;
+            m_parser.header_done = on_response_header_done;
+            m_parser.last_chunk = on_response_last_chunk;
+            m_parser.http_field = on_response_http_field;
+        }
+    }
+}
---
+++ http_parser.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <memory>
+#include <string>
+
+#include "http.h"
+#include "httprequest_parser.h"
+#include "httprespond_parser.h"
+
+namespace Framework {
+    namespace HTTP {
+        class HttpRequestParser {
+        public:
+            typedef std::shared_ptr<HttpRequestParser> ptr;
+            HttpRequestParser();
+            //size_t execute(const char* data, size_t len, size_t off);
+            size_t execute(char* data, size_t len);
+            int isFinished();
+            int hasError();
+
+            HttpRequest::ptr getData() const { return m_data; }
+
+            void setError(int v) { m_error = v; }
+        private:
+            http_parser m_parser;
+            HttpRequest::ptr m_data;
+            int m_error;
+        };
+
+        class HttpResponseParser {
+        public:
+            typedef std::shared_ptr<HttpResponseParser> ptr;
+            HttpResponseParser();
+            size_t execute(const char* data, size_t len, size_t off);
+            int isFinished();
+            int hasError();
+
+            HttpResponse::ptr getData() const { return m_data; }
+
+            void setError(int v) { m_error = v; }
+        private:
+            httpclient_parser m_parser;
+            HttpResponse::ptr m_data;
+            int m_error;
+        };
+    }
+}
